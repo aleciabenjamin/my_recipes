efcbd97671d61db09a232e200346eb7ca42cd20b
Alecia Benjamin <aleciabenjamin@gmail.com>
2020-03-05 15:51:52 +0100

    display title (label), calories and image

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Recipe from "./Recipe";
 
 const App = () => {
-	const APP_ID = "58d196b6";
-	const APP_KEY = "1eeceaf60c35452f88f31ffb7dfd424b";
+  const APP_ID = "58d196b6";
+  const APP_KEY = "1eeceaf60c35452f88f31ffb7dfd424b";
 
-	
-	useEffect( () => {
-		getRecipes();
+  const [recipes, setRecipes] = useState([]);
 
-	}, []);
+  useEffect(() => {
+    getRecipes();
+  }, []);
 
-	const getRecipes = async () => {
-		const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
-		const data = await response.json();
-		console.log(data.hits);
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
 
-	}
-
-	return(
-		<div className="App">
-			<form className="search-form">
-				<input className="search-bar" type="text" />
-				<button className="search-button" type="submit">Search</button>
-			</form>
-		</div>
-	)
-}
+  return (
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      {recipes.map(recipe => (
+        <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default App;
